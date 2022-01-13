7de400909fac630c033c97f9b634fa8ca0836f0b
Naddiya <nadiamedkouriz@gmail.com>
2022-01-13 16:31:37 +0100

    refactor: seperate increment decrement

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
 
-    const [breakLength, setBreakLength] = useState(5);
-    const [sessionLenght, setSessionLenght] = useState(25);
     const [minutes, setMinutes] = useState(25);
-    const [seconds, setSeconds] = useState(0);
+    const [seconds, setSeconds] = useState(parseInt(0, 10));
+    const [breakLength, setBreakLength] = useState(5);
+    const [sessionLength, setSessionLength] = useState(25);
     const [timer, setTimer] = useState(false);
     const [reset, setReset] = useState(false);
-    const [session, switchSession] = useState(true);
     const [warning, setWarning] = useState(false);
+    const [session, setSession] = useState("Session");
+    const [playing, setPlaying] = useState(false);
 
-    const updateTimer = (value) => {
+    const startStopTimer = (value) => {
         if (value === "start") {
             setTimer(true);
         } else if (value === "stop") {
@@ -20,26 +21,55 @@ function App() {
             setReset(true);
         }
     };
-
-    const increment = (value) => {
-        if (value === 'break-increment' && breakLength < 59) {
-            !session && setMinutes(minutes + 1);
+    const incrementSession = (e) => {
+        if (!timer && sessionLength < 60) {
+            setSessionLength(sessionLength + 1);
+            if (session === "Session") {
+                if (seconds !== 0) {
+                    setSeconds(0);
+                } else if (seconds === 0) {
+                    setMinutes(minutes + 1);
+                }
+            }
+        }
+    };
+    const decrementSession = () => {
+        if (!timer && sessionLength > 1) {
+            setSessionLength(sessionLength - 1);
+            if (session === "Session") {
+                if (seconds !== 0) {
+                    setSeconds(0);
+                } else if (seconds === 0) {
+                    setMinutes(minutes - 1);
+                }
+            }
+        }
+    };
+    const incrementBreak = () => {
+        if (!timer && breakLength < 60) {
             setBreakLength(breakLength + 1);
-        } else if (value === 'session-increment' && sessionLenght < 59) {
-            session && setSessionLenght(sessionLenght + 1);
-            setMinutes(minutes + 1);
+            if (session === "Break") {
+                if (seconds !== 0) {
+                    setSeconds(0);
+                } else if (seconds === 0) {
+                    setMinutes(minutes + 1);
+                }
+            }
         }
     };
-    const decrement = (value) => {
-        if (value === 'break-decrement' && breakLength >= 1) {
-            !session && setMinutes(minutes - 1);
+    const decrementBreak = () => {
+        if (!timer && breakLength > 1) {
             setBreakLength(breakLength - 1);
-        } else if (value === "session-decrement" && sessionLenght >= 1) {
-            session && setSessionLenght(sessionLenght - 1);
-            setMinutes(minutes - 1);
+            if (session === "Break") {
+                if (seconds !== 0) {
+                    setSeconds(0);
+                } else if (seconds === 0) {
+                    setMinutes(minutes - 1);
+                }
+            }
         }
     };
-
+    // countdown rules
     useEffect(() => {
         if (timer) {
             const interval = setInterval(() => {
@@ -50,40 +80,48 @@ function App() {
                     setSeconds(seconds - 1);
                 } else if (minutes > 0) {
                     setMinutes(minutes - 1);
-                } else if (session && minutes === 0 && seconds === 0) {
-                    switchSession(false);
-                    setMinutes(breakLength);
-                } else if (!session && minutes === 0 && seconds === 0) {
-                    switchSession(true);
-                    setMinutes(sessionLenght);
+                } else if (minutes === 0 && seconds === 0) {
+                    setPlaying(true);
+                    setTimer(false);
+                    if (session === "Session") {
+                        setSession("Break");
+                        setMinutes(breakLength);
+                        setTimer(true);
+                    } else if (session === "Break") {
+                        setSession("Session");
+                        setMinutes(sessionLength);
+                        setTimer(true);
+                    }
+                    setPlaying(false);
                 }
             }, 1000);
             return () => clearInterval(interval);
         }
-    }, [breakLength, minutes, seconds, session, sessionLenght, timer, warning]);
-
+    }, [breakLength, minutes, seconds, session, sessionLength, timer, warning]);
+    // reset rules
     useEffect(() => {
         if (reset) {
             setTimer(false);
-            setSeconds(0);
+            setSessionLength(25);
             setBreakLength(5);
-            setSessionLenght(25);
             setMinutes(25);
-            switchSession(true);
-            setTimeout(setReset(false), 500);
+            setSeconds(0);
+            setSession("Session");
+            setPlaying(false);
         }
+        setTimeout(setReset(false), 200);
     }, [breakLength, reset]);
-
+    // warning if one second left
     useEffect(() => {
         minutes < 1 ? setWarning(true) : setWarning(false);
-    }, [minutes, warning]);
-
+    }, [minutes, seconds, warning]);
+    // play sound
     useEffect(() => {
-        if (minutes === 0 && seconds === 0) {
-            const audio = new Audio("https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav");
-            audio.play();
-        }
-    }, [minutes, seconds]);
+        const audio = new Audio("https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav");
+        playing ? audio.play() : audio.pause();
+    },
+        [playing]
+    );
 
     return (
         <div className="clock">
@@ -91,56 +129,46 @@ function App() {
             <div className="clock-controls">
                 <div id="break-label">
                     Break Length
-                    <div className="clock-controls-break">
+                    <div className="clock-controls-break" >
                         <button className="clock-controls-btn">
-                            <i className="fa fa-arrow-down fa-3x" id="break-decrement" onClick={(e) => { decrement(e.target.id); }}></i>
+                            <i className="fa fa-arrow-down fa-3x" id="break-decrement" onClick={decrementBreak}></i>
                         </button>
-                        <div className="clock-controls-break-display">
+                        <div id="break-length" className="clock-controls-break-display">
                             {breakLength}
                         </div>
                         <button className="clock-controls-btn" >
-                            <i className="fa fa-arrow-up fa-3x" id="break-increment" onClick={(e) => { increment(e.target.id); }}></i>
+                            <i className="fa fa-arrow-up fa-3x" id="break-increment" onClick={incrementBreak}></i>
                         </button>
                     </div>
                 </div>
                 <div id="session-label">
                     Session Length
                     <div className="clock-controls-session">
-                        <button className="clock-controls-btn" >
-                            <i id="session-decrement" onClick={(e) => { decrement(e.target.id); }} className="fa fa-arrow-down fa-3x"></i>
+                        <button className="clock-controls-btn"  >
+                            <i id="session-decrement" onClick={decrementSession} className="fa fa-arrow-down fa-3x"></i>
                         </button>
-                        <div className="clock-controls-session-display">
-                            {sessionLenght}
+                        <div id="session-length" className="clock-controls-session-display">
+                            {sessionLength}
                         </div>
-                        <button className="clock-controls-btn">
-                            <i className="fa fa-arrow-up fa-3x" id="session-increment" onClick={(e) => { increment(e.target.id); }}></i>
+                        <button className="clock-controls-btn" onClick={incrementSession}>
+                            <i className="fa fa-arrow-up fa-3x" id="session-increment" value="+"></i>
                         </button>
                     </div>
                 </div>
             </div>
             <div className={`clock-display ${warning && "warning"}`}>
                 <div id="timer-label" className="clock-display-title">
-                    {session === true ? "Session" : "Break"}
+                    {session}
                 </div>
                 <div id="time-left" className="clock-display-time">
-                    {minutes < 10 ? `0${minutes}` : minutes}
-                    :
-                    {seconds < 10 ? `0${seconds}` : seconds}
-
+                    {minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
                 </div>
             </div>
             <div id="start_stop" className="clock-icons">
-                <i className="fa fa-play-circle fa-3x" id="start" onClick={(e) => { updateTimer(e.target.id); }}></i>
-                <i className="fa fa-pause-circle fa-3x" id="stop" onClick={(e) => { updateTimer(e.target.id); }}></i>
-                <i className="fa fa-refresh fa-3x" id="reset" onClick={(e) => { updateTimer(e.target.id); }}></i>
-                <audio
-                    id="beep"
-                    preload="auto"
-                    src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
-                >
-                </audio>
+                <i className="fa fa-play-circle fa-3x" id="start" onClick={(e) => { startStopTimer(e.target.id); }}></i>
+                <i className="fa fa-pause-circle fa-3x" id="stop" onClick={(e) => { startStopTimer(e.target.id); }}></i>
+                <i className="fa fa-refresh fa-3x" id="reset" onClick={(e) => { startStopTimer(e.target.id); }}></i>
             </div>
-
             <div className="clock-signature">
                 <p>
                     Designed by Peter Weinberg
@@ -151,6 +179,6 @@ function App() {
             </div>
         </div>
     );
-}
+};
 
 export default App;
